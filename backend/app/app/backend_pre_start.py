c38a9bb4c9ee85e8d4a3378cda5219792e70887c
a.levakin <a.levakin@napoleonit.ru>
2020-06-01 17:13:11 +0500

    add logging setup

--- backend/app/app/backend_pre_start.py
+++ backend/app/app/backend_pre_start.py
@@ -1,14 +1,56 @@
+import json
 import logging
+import os
+import sys
+from enum import Enum
+
+import structlog
+from structlog.contextvars import merge_contextvars
+from tenacity import after_log, before_log, retry, stop_after_attempt, wait_fixed
 
 from app.db.session import SessionLocal
-from tenacity import (after_log, before_log, retry, stop_after_attempt,
-                      wait_fixed)
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+logger = structlog.getLogger(__name__)
 
 max_tries = 60 * 5  # 5 minutes
 wait_seconds = 1
+
+
+class LogFormat(str, Enum):
+    JSON = "json"
+    CONSOLE = "console"
+
+
+log_level = os.environ.get("LOG_LEVEL", logging.INFO)
+if isinstance(log_level, str):
+    log_level = log_level.upper()
+
+logging.basicConfig(format="%(message)s", stream=sys.stdout, level=log_level)
+
+log_format = LogFormat(os.environ.get("LOG_FORMAT", default=LogFormat.JSON.value))
+
+if log_format == LogFormat.JSON:
+    renderer = structlog.processors.JSONRenderer(serializer=json.dumps)
+else:
+    renderer = structlog.dev.ConsoleRenderer()
+
+structlog.configure(
+    processors=[
+        merge_contextvars,
+        structlog.stdlib.filter_by_level,
+        structlog.stdlib.add_logger_name,
+        structlog.stdlib.add_log_level,
+        structlog.stdlib.PositionalArgumentsFormatter(),
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.processors.StackInfoRenderer(),
+        structlog.dev.set_exc_info,
+        structlog.processors.format_exc_info,
+        renderer,
+    ],
+    logger_factory=structlog.stdlib.LoggerFactory(),
+    wrapper_class=structlog.stdlib.BoundLogger,
+    cache_logger_on_first_use=True,
+)
 
 
 @retry(

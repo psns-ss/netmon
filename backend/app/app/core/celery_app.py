3b02c63ff6d29d246dd230caabda2c3e86f77e98
a.levakin <a.levakin@napoleonit.ru>
2020-06-03 02:33:30 +0500

    periodic ping of all machines

--- backend/app/app/core/celery_app.py
+++ backend/app/app/core/celery_app.py
@@ -2,4 +2,14 @@ from celery import Celery
 
 celery_app = Celery("worker", broker="amqp://guest@queue//")
 
-celery_app.conf.task_routes = {"app.worker.test_celery": "main-queue"}
+celery_app.conf.task_routes = {
+    "app.worker.test_celery": "main-queue",
+    "app.worker.check_machines_statuses": "main-queue",
+}
+
+celery_app.conf.beat_schedule = {
+    "check-machines-periodically": {
+        "task": "app.worker.check_machines_statuses",
+        "schedule": 30.0,
+    },
+}
--- backend/app/app/worker.py
+++ backend/app/app/worker.py
@@ -1,11 +1,42 @@
+import asyncio
+
 import sentry_sdk
+import structlog
+from app import crud
 from app.core.celery_app import celery_app
 from app.core.config import settings
+from app.db.session import SessionLocal
 from sentry_sdk.integrations.celery import CeleryIntegration
 
 sentry_sdk.init(dsn=settings.SENTRY_DSN, integrations=[CeleryIntegration()])
+
+logger = structlog.get_logger()
 
 
 @celery_app.task(acks_late=True)
 def test_celery(word: str) -> str:
     return f"test task return {word}"
+
+
+@celery_app.task
+def check_machines_statuses() -> None:
+    logger.debug("started check machine statuses")
+    skip = 0
+    limit = 100
+    db = SessionLocal()
+    try:
+        while True:
+            checked_machines = asyncio.run(
+                crud.machine.get_multi_with_check_online(db, skip=skip, limit=limit)
+            )
+
+            if not checked_machines:
+                break
+
+            logger.debug(f"checked {len(checked_machines)} machines")
+
+            skip += limit
+
+        logger.debug("checked all machines")
+    finally:
+        db.close()
